fd1da04c6f87d2574f6c9935a73a6fb7132a4ef2
raijel <ngcthonym42@gmail.com>
2023-09-21 14:12:54 -0400

    typegoose

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 import { REDIS_HOST, REDIS_PORT, REDIS_PASS } from "../config.js";
 
 export const redisClient = createClient({
-  url: `redis://:${REDIS_PASS}@${REDIS_HOST}:${REDIS_PORT}`
+  url: `redis://:${REDIS_PASS}@${REDIS_HOST}:${REDIS_PORT}`,
 });
 
 export const connectRedis = async () => {
--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -1,5 +1,6 @@
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
+import { LOCAL_PORT } from "../config.js";
 
 const options = {
   definition: {
@@ -7,11 +8,12 @@ const options = {
     info: {
       title: "social network API",
       version: "1.0.0",
-      description: "A simple social network API made by nodejs-express, mongoDB Atlas and Cloudinary as a cloud service image storage.",
+      description:
+        "Simple social network API made by nodejs-express.",
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: `http://localhost:${LOCAL_PORT}`,
       },
     ],
   },
@@ -21,3 +23,5 @@ const options = {
 const specs = swaggerJsDoc(options);
 export const swaggerServe = swaggerUI.serve;
 export const swaggerSetup = swaggerUI.setup(specs);
+
+
